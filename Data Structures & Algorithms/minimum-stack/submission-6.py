d89bebbaa64a720c8570b6e4178c55556e270e76
Cristiano Pinto <140563307+crowmoed@users.noreply.github.com>
2026-05-25 03:52:07 -0400

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    data = []
+
+    def __init__(self):
+        self.data = []
+        
+
+    def push(self, val: int) -> None:
+        self.data.append(val)
+
+    def pop(self) -> None:
+        self.data = self.data[0:-1]
+
+    def top(self) -> int:
+        return self.data[-1]
+
+    def getMin(self) -> int:
+        return min(self.data)
+        
